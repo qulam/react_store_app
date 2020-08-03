8dbf2f45d5c3a7a47e976e1cb950eff543225d92
Qulam Alisoy <gulam@onneks.com>
2020-08-03 23:17:02 +0400

    REFACTORING---------CHANGE CONTAINER/APP CLASS BASED COMPONENT TO FUNCTIONAL COMPONENT!

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,11 @@
 import React, {Component, useEffect, useState} from 'react';
 import {Redirect, Route, Switch} from 'react-router-dom';
-import {connect, useDispatch, useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import axios from '../util/Api';
 import '../styles/App.css';
 import MainApp from '../app/index';
 import SignIn from './Signin';
-import {forceQuit, setInitUrl} from '../actions/Auth';
+import {forceQuit,} from '../actions/Auth';
 import asyncComponent from '../util/asyncComponent';
 import CircularProgress from "../components/CircularProgress";
 
@@ -58,35 +58,28 @@ const RestrictedRoute = ({component: Component, authUser, token, ...rest}) => {
 };
 
 
-class App extends Component {
+const App = () => {
 
-    render() {
-        const {match, location, access_token, authUser,} = this.props;
-        if (location.pathname === '/') {
-            if (access_token === null || !authUser) {
-                return (<Redirect to={'/signin'}/>);
-            } else {
-                return (<Redirect to={'/app/dashboard'}/>);
-            }
+    const {access_token, authUser,} = useSelector(state => state.auth);
+    if (window.location.pathname === '/') {
+        if (access_token === null || !authUser) {
+            return (<Redirect to={'/signin'}/>);
+        } else {
+            return (<Redirect to={'/app/dashboard'}/>);
         }
-
-        return (
-            <div className="app-main">
-                <Switch>
-                    <RestrictedRoute path={`/app`} authUser={authUser} token={access_token}
-                                     component={MainApp}/>
-                    <Route path='/signin' component={SignIn}/>
-                    <Route
-                        component={asyncComponent(() => import('../components/Error404'))}/>
-                </Switch>
-            </div>
-        );
     }
-}
 
-const mapStateToProps = ({auth}) => {
-    const {authUser, token, initURL} = auth;
-    return {authUser, token, initURL};
+    return (
+        <div className="app-main">
+            <Switch>
+                <RestrictedRoute path={`/app`} authUser={authUser} token={access_token}
+                                 component={MainApp}/>
+                <Route path='/signin' component={SignIn}/>
+                <Route
+                    component={asyncComponent(() => import('../components/Error404'))}/>
+            </Switch>
+        </div>
+    );
 };
 
-export default connect(mapStateToProps, {setInitUrl})(App);
+export default App;

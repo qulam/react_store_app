63c23ea25cc56bccde0b2e02135d1b6e6fcd6c1d
Qulam Alisoy <gulam@onneks.com>
2020-08-04 19:40:52 +0400

    Fix update state unmounted components error

--- src/components/Error404/index.js
+++ src/components/Error404/index.js
@@ -1,19 +1,6 @@
 import React, {useEffect} from 'react';
-import {useDispatch} from "react-redux";
-import {TOGGLE_BUCKET} from "../../constants/ActionTypes";
 
 const Error404 = () => {
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        /*Disable bucket when component un mount.*/
-        dispatch({type: TOGGLE_BUCKET, payload: false});
-
-        return () => {
-            /*Enable bucket when component un mount.*/
-            dispatch({type: TOGGLE_BUCKET, payload: true});
-        }
-    }, []);
 
     return (
         <div className="container">
